32c498c6c547afff4ef478307c46063b4c0626e0
Lorenzo Calosso <c.lory1999@gmail.com>
2023-06-26 11:09:58 +0200

    centralized rotated all train

--- centralized_training_rotated.py
+++ centralized_training_rotated.py
@@ -109,7 +109,7 @@ def get_train_test_dataloader(domain_out, train_batch_size, test_batch_size=256)
     train_data , test_data = read_femnist_data(train_data_dir, test_data_dir)
     #test data in this case is not utilized because as test set we choose one of the six rotation domain of the trainset
     train_datasets, test_datasets = [], []
-    '''
+    ''' ### da cancellare (primo metodo ruotando a priori)
     rotation = [0, 15, 30, 45, 60, 75]
     data = []
     n_clients_per_set = int(round(1002 / 6, 0))
@@ -132,35 +132,53 @@ def get_train_test_dataloader(domain_out, train_batch_size, test_batch_size=256)
     cont_clients = 0
     for user, data in train_data.items():
         if cont_clients >= n_clients_total:
-            break
+            if domain_out != None:
+                break
+            else:
+                train_transforms = nptr.Compose([
+                    nptr.ToTensor(),
+                    nptr.Normalize((0.5,), (0.5,)),
+                ])
+                train_datasets.append(Femnist(data, train_transforms, user))
+                continue
         train_transforms = nptr.Compose([
             nptr.ToTensor(),
             sstr.RandomRotation(rotation[int(cont_clients / n_clients_per_set)]),
             nptr.Normalize((0.5,), (0.5,)),
         ])
         cont_clients += 1
-        train_datasets.append(Femnist(data, train_transforms, user))   
-    #for user, data in test_data.items():
-    #    test_datasets.append(Femnist(data, test_transforms, user))
+        train_datasets.append(Femnist(data, train_transforms, user))  
 
-    test_datasets = train_datasets[int(domain_out*len(train_datasets)/6) : int((domain_out+1)*len(train_datasets)/6)]
-    del train_datasets[int(domain_out*len(train_datasets)/6) : int((domain_out+1)*len(train_datasets)/6)]
+    if domain_out == None:
+        test_transforms = nptr.Compose([
+            nptr.ToTensor(),
+            nptr.Normalize((0.5,), (0.5,))
+        ])
+        for user, data in test_data.items():
+            test_datasets.append(Femnist(data, test_transforms, user))
+    else:
+        test_datasets = train_datasets[int(domain_out*len(train_datasets)/6) : int((domain_out+1)*len(train_datasets)/6)]
+        del train_datasets[int(domain_out*len(train_datasets)/6) : int((domain_out+1)*len(train_datasets)/6)]
     
     all_train_data = defaultdict(lambda: {})
+    all_train_data['x'] = []
+    all_train_data['y'] = []
     for femn in train_datasets:
         for i in range(femn.__len__()):
-            image, label = femn[i]
-            all_train_data['x'] = image
-            all_train_data['y'] = label
+            image, label = femn[i] # = femn.__getitem__[i]
+            all_train_data['x'].append(image)
+            all_train_data['y'].append(label)
     empty_transforms = nptr.Compose()
     train_datasets = Femnist(all_train_data, empty_transforms, user = 0)
     
     all_test_data = defaultdict(lambda: {})
+    all_test_data['x'] = []
+    all_test_data['y'] = []
     for femn in test_datasets:
         for i in range(femn.__len__()):
             image, label = femn[i]
-            all_test_data['x'] = image
-            all_test_data['y'] = label
+            all_test_data['x'].append(image)
+            all_test_data['y'].append(label)
     empty_transforms = nptr.Compose()
     test_datasets = Femnist(all_test_data, empty_transforms, user = 0)
 
@@ -176,7 +194,7 @@ def main(domain_out = None, batch_size=64, device='cuda:0', learning_rate=10**-2
     
     wandb.init(
         project = "Rotated_centralized",
-        name = "domain_out=" + str(domain_out*15) + "°",
+        name = "domain_out = " + str(domain_out*15) + "°",
         config = {
         "batch_size": batch_size,
         "learning_rate": learning_rate,
@@ -212,7 +230,8 @@ def main(domain_out = None, batch_size=64, device='cuda:0', learning_rate=10**-2
 
 if __name__ == '__main__':
        
-    domain_target_set = [0, 1, 2, 3, 4, 5] # 0°, 15°, 30°, 45°, 60°, 75°
+    domain_target_set = [0, 1, 2, 3, 4, 5, None] # 0°, 15°, 30°, 45°, 60°, 75°, None (to run the centralized on all 
+                                                 # the train set in which only 1000 clients are rotated and using as test the testset)
     wandb.login()
 
     # 6 runs in which one of the 6 rotation domain is left out and used as test set
